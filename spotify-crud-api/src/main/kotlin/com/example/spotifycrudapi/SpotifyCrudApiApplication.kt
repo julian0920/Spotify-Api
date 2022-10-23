71aaf2faa0b5c94895390360190cb6da123a68a9
julianhenkel <70511865+julianhenkel@users.noreply.github.com>
2022-10-23 22:09:47 +0200

    use CommandLineRunner to save data to the memory database when application starts

--- spotify-crud-api/src/main/kotlin/com/example/spotifycrudapi/SpotifyCrudApiApplication.kt
+++ spotify-crud-api/src/main/kotlin/com/example/spotifycrudapi/SpotifyCrudApiApplication.kt
@@ -1,15 +1,37 @@
 package com.example.spotifycrudapi
 
+import com.example.spotifycrudapi.repositories.AlbumRepository
+import com.example.spotifycrudapi.repositories.ArtistRepository
+import com.example.spotifycrudapi.spotify.service.SpotifyArtistService
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.boot.runApplication
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
 @SpringBootApplication
-@EntityScan(basePackages = ["com/example/spotifycrudapi/data"])
+@EntityScan(basePackages = ["com/example/spotifycrudapi/model"])
 @EnableJpaRepositories(basePackages = ["com/example/spotifycrudapi/repositories"])
-class SpotifyCrudApiApplication
+class SpotifyCrudApiApplication : CommandLineRunner {
+
+    @Autowired
+    val spotifyArtistService = SpotifyArtistService()
+
+    @Autowired
+    lateinit var albumRepository: AlbumRepository
+
+    @Autowired
+    lateinit var artistRepository: ArtistRepository
+
+    override fun run(vararg args: String?) {
+        albumRepository.saveAll(spotifyArtistService.getArtistAlbumById("4zYX8Aa744hQ5O2hpAYQI3"))
+        artistRepository.saveAll(spotifyArtistService.getSeveralArtistsByIds())
+        println("-------------Saved Data into H2 Database-------------")
+    }
+}
+
 
 fun main(args: Array<String>) {
-	runApplication<SpotifyCrudApiApplication>(*args)
+    runApplication<SpotifyCrudApiApplication>(*args)
 }
