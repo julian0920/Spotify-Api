c110826ad364995c33dfae2fdfd1ce031cf99c17
julianhenkel <70511865+julianhenkel@users.noreply.github.com>
2022-10-23 22:13:25 +0200

    implement generic helper method to map from a List to a destination Class

---
+++ spotify-crud-api/src/main/kotlin/com/example/spotifycrudapi/spotify/helper/DozerMappingHelper.kt
@@ -0,0 +1,24 @@
+package com.example.spotifycrudapi.spotify.helper
+
+import com.example.spotifycrudapi.spotify.config.DozerMappingConfig
+import com.github.dozermapper.core.DozerBeanMapperBuilder
+
+class DozerMappingHelper {
+
+    private val mapper =
+        DozerBeanMapperBuilder.create().withMappingBuilder(DozerMappingConfig().beanMappingBuilder()).build()
+
+    /**
+     * Generic Helper Method to map from a List of Type <code> F </code> to a List of Type <code> T </code>
+     * @param source the source List of type <code> F </code> you want to map from
+     * @param destination the Class of type <code> T </code> you want to map to
+     * @return the mapped object as a list of type <code> T </code>
+     */
+    fun <F, T> mapToList(source: List<F>, destination: Class<T>): List<T> {
+        val destinationObject: MutableList<T> = ArrayList()
+        for (element: F in source) {
+            destinationObject.add(mapper.map(element, destination))
+        }
+        return destinationObject
+    }
+}
