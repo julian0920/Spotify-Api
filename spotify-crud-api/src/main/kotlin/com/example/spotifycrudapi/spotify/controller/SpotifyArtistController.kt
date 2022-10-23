40a35891624ab068d780298634c33350ba16ba9d
julianhenkel <70511865+julianhenkel@users.noreply.github.com>
2022-10-23 22:14:16 +0200

    Artist Controller and Service to get Artists and their albums from the spotify Api

---
+++ spotify-crud-api/src/main/kotlin/com/example/spotifycrudapi/spotify/controller/SpotifyArtistController.kt
@@ -0,0 +1,34 @@
+package com.example.spotifycrudapi.spotify.controller
+
+import com.example.spotifycrudapi.model.Album
+import com.example.spotifycrudapi.model.Artist
+import com.example.spotifycrudapi.spotify.service.SpotifyArtistService
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.ResponseBody
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class SpotifyArtistController {
+
+    @Autowired
+    private val spotifyArtistService = SpotifyArtistService()
+
+    @GetMapping("/artists", produces = [MediaType.APPLICATION_JSON_VALUE])
+    @ResponseBody
+    fun getSeveralArtists(): ResponseEntity<List<Artist>> {
+        val severalArtistsByIds = spotifyArtistService.getSeveralArtistsByIds()
+        return ResponseEntity(severalArtistsByIds, HttpStatus.OK)
+    }
+
+    @GetMapping("/artists/{id}/albums", produces = [MediaType.APPLICATION_JSON_VALUE])
+    @ResponseBody
+    fun getArtistsAlbums(@PathVariable("id") artistId: String): ResponseEntity<List<Album>> {
+        val artistAlbumsById = spotifyArtistService.getArtistAlbumById(artistId)
+        return ResponseEntity(artistAlbumsById, HttpStatus.OK)
+    }
+}
---
+++ spotify-crud-api/src/main/kotlin/com/example/spotifycrudapi/spotify/service/SpotifyArtistService.kt
@@ -0,0 +1,58 @@
+package com.example.spotifycrudapi.spotify.service
+
+import com.example.spotifycrudapi.model.Album
+import com.example.spotifycrudapi.model.Artist
+import com.example.spotifycrudapi.spotify.authorization.SpotifyAuthorization
+import com.example.spotifycrudapi.spotify.helper.DozerMappingHelper
+import mu.KotlinLogging
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Service
+import se.michaelthelin.spotify.exceptions.SpotifyWebApiException
+import java.io.IOException
+
+@Service
+class SpotifyArtistService {
+    // 10 Artist Ids
+    private val artistIds = "4zYX8Aa744hQ5O2hpAYQI3,37awA8DFCAnCCL7aqYbDnD," +
+            "1Cs0zKBU1kc0i8ypK3B9ai,73jBynjsVtofjRpdpRAJGk,0CbeG1224FS58EUx4tPevZ," +
+            "2vVNxGBvKRQMWwI5c8KmYh,3gk0OYeLFWYupGFRHqLSR7,5eHs2hHfUzGizdnrLjc3CW," +
+            "77AiFEVeAVj2ORpC85QVJs,2o5jDhtHVPhrJdv3cEQ99Z"
+
+    private val logger = KotlinLogging.logger {}
+
+    private val mapper = DozerMappingHelper()
+
+    @Autowired
+    private val spotifyAuthorization = SpotifyAuthorization()
+
+    @Autowired
+    private val spotifyService = SpotifyService()
+
+
+    fun getSeveralArtistsByIds(): List<Artist> {
+        val spotifyApi = spotifyService.clientCredentialsSync(spotifyAuthorization.createSpotifyApi())
+        try {
+            val artistList = spotifyApi.getSeveralArtists(artistIds).build().execute().asList()
+            return mapper.mapToList(artistList, Artist::class.java)
+        } catch (ioException: IOException) {
+            logger.error("Error {}", ioException.localizedMessage)
+        } catch (swae: SpotifyWebApiException) {
+            logger.error("Spotify Web Api Exception {}", swae.localizedMessage)
+        }
+        return emptyList()
+    }
+
+    fun getArtistAlbumById(artistId: String): List<Album> {
+        val spotifyApi = spotifyService.clientCredentialsSync(spotifyAuthorization.createSpotifyApi())
+        try {
+            val albumSimplifiedList = spotifyApi.getArtistsAlbums(artistId).album_type("album,single").limit(10).build()
+                .execute().items.asList()
+            return mapper.mapToList(albumSimplifiedList, Album::class.java)
+        } catch (ioException: IOException) {
+            logger.error("Error {}", ioException.localizedMessage)
+        } catch (swae: SpotifyWebApiException) {
+            logger.error("Spotify Web Api Exception {}", swae.localizedMessage)
+        }
+        return emptyList()
+    }
+}
