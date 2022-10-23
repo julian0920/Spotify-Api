6139a6885970c5c82f2dadd8d3945c0f54843900
julianhenkel <70511865+julianhenkel@users.noreply.github.com>
2022-10-23 22:15:09 +0200

    annotate with Spring annotation

--- spotify-crud-api/src/main/kotlin/com/example/spotifycrudapi/repositories/AlbumRepository.kt
+++ spotify-crud-api/src/main/kotlin/com/example/spotifycrudapi/repositories/AlbumRepository.kt
@@ -1,7 +1,9 @@
 package com.example.spotifycrudapi.repositories
 
-import com.example.spotifycrudapi.data.Album
+import com.example.spotifycrudapi.model.Album
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
 
+@Repository
 interface AlbumRepository : JpaRepository<Album, Long> {
 }
--- spotify-crud-api/src/main/kotlin/com/example/spotifycrudapi/repositories/ArtistRepository.kt
+++ spotify-crud-api/src/main/kotlin/com/example/spotifycrudapi/repositories/ArtistRepository.kt
@@ -1,7 +1,9 @@
 package com.example.spotifycrudapi.repositories
 
-import com.example.spotifycrudapi.data.Artist
+import com.example.spotifycrudapi.model.Artist
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
 
+@Repository
 interface ArtistRepository : JpaRepository<Artist, Long> {
 }
