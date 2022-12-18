8d9b3457c7207935146b247033d7fa61f36130b7
julianhenkel <jhenkel@immomio.de>
2022-12-18 18:35:08 +0100

    use dependency injection, store ids in constant

--- spotify-crud-api/src/main/kotlin/com/example/spotifycrudapi/spotify/service/SpotifyArtistService.kt
+++ spotify-crud-api/src/main/kotlin/com/example/spotifycrudapi/spotify/service/SpotifyArtistService.kt
@@ -5,34 +5,29 @@ import com.example.spotifycrudapi.model.Artist
 import com.example.spotifycrudapi.spotify.authorization.SpotifyAuthorization
 import com.example.spotifycrudapi.spotify.helper.DozerMappingHelper
 import mu.KotlinLogging
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 import se.michaelthelin.spotify.exceptions.SpotifyWebApiException
 import java.io.IOException
 
-@Service
-class SpotifyArtistService {
-    // 10 Artist Ids
-    private val artistIds = "4zYX8Aa744hQ5O2hpAYQI3,37awA8DFCAnCCL7aqYbDnD," +
-            "1Cs0zKBU1kc0i8ypK3B9ai,73jBynjsVtofjRpdpRAJGk,0CbeG1224FS58EUx4tPevZ," +
-            "2vVNxGBvKRQMWwI5c8KmYh,3gk0OYeLFWYupGFRHqLSR7,5eHs2hHfUzGizdnrLjc3CW," +
-            "77AiFEVeAVj2ORpC85QVJs,2o5jDhtHVPhrJdv3cEQ99Z"
+private const val ARTIST_IDS = "4zYX8Aa744hQ5O2hpAYQI3,37awA8DFCAnCCL7aqYbDnD," +
+        "1Cs0zKBU1kc0i8ypK3B9ai,73jBynjsVtofjRpdpRAJGk,0CbeG1224FS58EUx4tPevZ," +
+        "2vVNxGBvKRQMWwI5c8KmYh,3gk0OYeLFWYupGFRHqLSR7,5eHs2hHfUzGizdnrLjc3CW," +
+        "77AiFEVeAVj2ORpC85QVJs,2o5jDhtHVPhrJdv3cEQ99Z"
 
-    private val logger = KotlinLogging.logger {}
+@Service
+class SpotifyArtistService(
+    private val spotifyAuthorization: SpotifyAuthorization,
+    private val spotifyService: SpotifyService,
+) {
 
     private val mapper = DozerMappingHelper()
-
-    @Autowired
-    private val spotifyAuthorization = SpotifyAuthorization()
-
-    @Autowired
-    private val spotifyService = SpotifyService()
+    private val logger = KotlinLogging.logger {}
 
 
     fun getSeveralArtistsByIds(): List<Artist> {
         val spotifyApi = spotifyService.clientCredentialsSync(spotifyAuthorization.createSpotifyApi())
         try {
-            val artistList = spotifyApi.getSeveralArtists(artistIds).build().execute().asList()
+            val artistList = spotifyApi.getSeveralArtists(ARTIST_IDS).build().execute().asList()
             return mapper.mapToList(artistList, Artist::class)
         } catch (ioException: IOException) {
             logger.error("Error {}", ioException.localizedMessage)
