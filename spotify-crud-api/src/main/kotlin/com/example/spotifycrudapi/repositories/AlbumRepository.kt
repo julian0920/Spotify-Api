9ad0c89718e62ca9b56f5745eeb7987d54ee9a40
julianhenkel <jhenkel@immomio.de>
2022-12-18 19:28:57 +0100

    remove unused curly braces

--- spotify-crud-api/src/main/kotlin/com/example/spotifycrudapi/repositories/AlbumRepository.kt
+++ spotify-crud-api/src/main/kotlin/com/example/spotifycrudapi/repositories/AlbumRepository.kt
@@ -5,5 +5,4 @@ import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface AlbumRepository : JpaRepository<Album, Long> {
-}
+interface AlbumRepository : JpaRepository<Album, Long>
--- spotify-crud-api/src/main/kotlin/com/example/spotifycrudapi/repositories/ArtistRepository.kt
+++ spotify-crud-api/src/main/kotlin/com/example/spotifycrudapi/repositories/ArtistRepository.kt
@@ -5,5 +5,4 @@ import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface ArtistRepository : JpaRepository<Artist, Long> {
-}
+interface ArtistRepository : JpaRepository<Artist, Long>
