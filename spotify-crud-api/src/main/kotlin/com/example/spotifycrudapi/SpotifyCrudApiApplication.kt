5bad1205ecf0b16ea5e5412571f294918a4b5d62
julianhenkel <jhenkel@immomio.de>
2022-12-19 22:34:45 +0100

    fix package paths, use dependency injection

--- spotify-crud-api/src/main/kotlin/com/example/spotifycrudapi/SpotifyCrudApiApplication.kt
+++ spotify-crud-api/src/main/kotlin/com/example/spotifycrudapi/SpotifyCrudApiApplication.kt
@@ -3,7 +3,6 @@ package com.example.spotifycrudapi
 import com.example.spotifycrudapi.repositories.AlbumRepository
 import com.example.spotifycrudapi.repositories.ArtistRepository
 import com.example.spotifycrudapi.spotify.service.SpotifyArtistService
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.domain.EntityScan
@@ -11,18 +10,13 @@ import org.springframework.boot.runApplication
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
 @SpringBootApplication
-@EntityScan(basePackages = ["com/example/spotifycrudapi/model"])
-@EnableJpaRepositories(basePackages = ["com/example/spotifycrudapi/repositories"])
-class SpotifyCrudApiApplication : CommandLineRunner {
-
-    @Autowired
-    val spotifyArtistService = SpotifyArtistService()
-
-    @Autowired
-    lateinit var albumRepository: AlbumRepository
-
-    @Autowired
-    lateinit var artistRepository: ArtistRepository
+@EntityScan(basePackages = ["com.example.spotifycrudapi.model"])
+@EnableJpaRepositories(basePackages = ["com.example.spotifycrudapi.repositories"])
+class SpotifyCrudApiApplication(
+    private val spotifyArtistService: SpotifyArtistService,
+    private val albumRepository: AlbumRepository,
+    private val artistRepository: ArtistRepository
+) : CommandLineRunner {
 
     override fun run(vararg args: String?) {
         albumRepository.saveAll(spotifyArtistService.getArtistAlbumById("4zYX8Aa744hQ5O2hpAYQI3"))
