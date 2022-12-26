35c9c51374845a92d1dffd7d66f09b541fc3b181
julianhenkel <70511865+julianhenkel@users.noreply.github.com>
2022-12-26 23:51:50 +0100

    add Dtos to split api layer from persistence layer

---
+++ spotify-crud-api/src/main/kotlin/com/example/spotifycrudapi/model/AlbumDto.kt
@@ -0,0 +1,9 @@
+package com.example.spotifycrudapi.model
+
+data class AlbumDto(
+    val name: String,
+
+    val releaseDate: String?,
+
+    val type: String?
+)
---
+++ spotify-crud-api/src/main/kotlin/com/example/spotifycrudapi/model/ArtistDto.kt
@@ -0,0 +1,9 @@
+package com.example.spotifycrudapi.model
+
+data class ArtistDto(
+    val name: String,
+
+    val popularity: String?,
+
+    val genres: List<String>?
+)
