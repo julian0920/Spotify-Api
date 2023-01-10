e145b8e3bfa4fa93b8cd1fd687c60c916aac5244
julianhenkel <70511865+julianhenkel@users.noreply.github.com>
2023-01-10 23:26:40 +0100

    remove command line runner and add event listener in order to save entities to database

--- spotify-crud-api/src/main/kotlin/com/example/spotifycrudapi/SpotifyCrudApiApplication.kt
+++ spotify-crud-api/src/main/kotlin/com/example/spotifycrudapi/SpotifyCrudApiApplication.kt
@@ -4,9 +4,6 @@ import com.example.spotifycrudapi.persistence.Album
 import com.example.spotifycrudapi.persistence.Artist
 import com.example.spotifycrudapi.repositories.AlbumRepository
 import com.example.spotifycrudapi.repositories.ArtistRepository
-import com.example.spotifycrudapi.spotify.service.SpotifyAlbumService
-import com.example.spotifycrudapi.spotify.service.SpotifyArtistService
-import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.boot.runApplication
@@ -15,18 +12,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 @SpringBootApplication
 @EntityScan(basePackageClasses = [Album::class, Artist::class])
 @EnableJpaRepositories(basePackageClasses = [AlbumRepository::class, ArtistRepository::class])
-class SpotifyCrudApiApplication(
-    private val spotifyArtistService: SpotifyArtistService,
-    private val spotifyAlbumService: SpotifyAlbumService,
-    private val albumRepository: AlbumRepository,
-    private val artistRepository: ArtistRepository
-) : CommandLineRunner {
-
-    override fun run(vararg args: String?) {
-        albumRepository.saveAll(spotifyAlbumService.getArtistAlbumById("4zYX8Aa744hQ5O2hpAYQI3"))
-        artistRepository.saveAll(spotifyArtistService.getSeveralArtistsByIds())
-    }
-}
+class SpotifyCrudApiApplication
 
 fun main(args: Array<String>) {
     runApplication<SpotifyCrudApiApplication>(*args)
