8f6e0b3fb4e8ec6fee7ef8b31447872bd0b7c7fc
julian <jhenkel989@gmail.com>
2023-02-19 15:29:10 +0100

    modify startupApplication to always delete all existing entries before saving new ones and use check to avoid duplicate entries

--- src/main/kotlin/com/example/spotifycrudapi/StartupApplicationListener.kt
+++ src/main/kotlin/com/example/spotifycrudapi/StartupApplicationListener.kt
@@ -1,5 +1,7 @@
 package com.example.spotifycrudapi
 
+import com.example.spotifycrudapi.persistence.Album
+import com.example.spotifycrudapi.persistence.Artist
 import com.example.spotifycrudapi.repositories.AlbumRepository
 import com.example.spotifycrudapi.repositories.ArtistRepository
 import com.example.spotifycrudapi.spotify.service.SpotifyAlbumService
@@ -7,6 +9,7 @@ import com.example.spotifycrudapi.spotify.service.SpotifyArtistService
 import org.springframework.context.ApplicationListener
 import org.springframework.context.event.ContextRefreshedEvent
 import org.springframework.context.event.EventListener
+import org.springframework.data.domain.Example
 import org.springframework.stereotype.Component
 
 @Component
@@ -18,7 +21,26 @@ class StartupApplicationListener(
 ) : ApplicationListener<ContextRefreshedEvent> {
     @EventListener
     override fun onApplicationEvent(event: ContextRefreshedEvent) {
-        albumRepository.saveAll(spotifyAlbumService.getArtistAlbumById("4zYX8Aa744hQ5O2hpAYQI3"))
-        artistRepository.saveAll(spotifyArtistService.getSeveralArtistsByIds())
+        albumRepository.deleteAll()
+        artistRepository.deleteAll()
+
+        val artists = spotifyArtistService.getSeveralArtistsByIds()
+        val artistAlbums = spotifyAlbumService.getArtistAlbumById("4zYX8Aa744hQ5O2hpAYQI3")
+        artists.forEach { artist -> saveIfArtistNotExists(artist) }
+        artistAlbums.forEach { album -> saveIfAlbumNotExists(album) }
+    }
+
+    private fun saveIfArtistNotExists(artist: Artist) {
+        val exists: Boolean = artistRepository.exists(Example.of(artist))
+        if (!exists) {
+            artistRepository.save(artist)
+        }
+    }
+
+    private fun saveIfAlbumNotExists(album: Album) {
+        val exists: Boolean = albumRepository.exists(Example.of(album))
+        if (!exists) {
+            albumRepository.save(album)
+        }
     }
 }
